1c0bfbb6383ff5713e96ad1e945be4a4cc98f8c5
tadashimasuda <tmasu4260@gmail.com>
2020-09-01 16:48:03 +0900

    show

--- pages/record/_id/index.vue
+++ pages/record/_id/index.vue
@@ -1,11 +1,65 @@
 <template>
-    <section class="section no-top-pad">
-        詳細ページ
-    </section>
+  <section class="section no-top-pad">
+    詳細ページ
+    <div class="columns">
+      <!-- <h2 class="subtitle">{{ogpData.message}}</h2>
+      <img src="ogpData.url" alt="">-->
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    
-}
+  head() {
+    return {
+      title: "OGPサービス",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "OGPメッセージサービスの説明が入ります",
+        },
+        { hid: "og:type", property: "og:type", content: "article" },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "OGPメッセージのタイトル",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: "OGPメッセージサービスの説明が入ります",
+        },
+        { hid: "og:url", property: "og:url", content: "{詳細ページのURL}" },
+        { hid: "og:image", property: "og:image", content: this.ogpData.url },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: this.ogpData.url,
+        },
+        {
+          hid: "twitter:image:src",
+          name: "twitter:image:src",
+          content: `${this.ogpData.url}`,
+        },
+      ],
+    };
+  },
+  computed: {
+    ogpData() {
+      return this.$store.getters["ogpData"];
+    },
+  },
+  async fetch({ app, store, route }) {
+    const id = route.params.id;
+    console.log(route);
+    await store.dispatch("getOPG", id);
+    return;
+  },
+};
 </script>
--- store/index.js
+++ store/index.js
@@ -15,17 +15,17 @@ export const mutations = {
     // methods
     setMessage(state, payload) {
         state.comment = payload.messsage,
-        state.area = payload.area,
-        state.way = payload.way,
-        state.count = payload.count,
-        state.time = payload.time,
-        state.reward = payload.reward
+            state.area = payload.area,
+            state.way = payload.way,
+            state.count = payload.count,
+            state.time = payload.time,
+            state.reward = payload.reward
     },
     setLoading(state, payload) {
         state.isLoading = payload;
     },
-    setOGP(state,payload){
-        state.ogpData=payload
+    setOGP(state, payload) {
+        state.ogpData = payload;
     }
 };
 
@@ -34,13 +34,24 @@ export const actions = {
     async setMessage({ commit }, payload) {
         console.log(payload);
         const id = await this.$axios.$post("/api/records", payload);
-        if(id){
-          this.$router.push(`/records/${id}/`);
+        if (id) {
+            this.$router.push(`/records/${id}/`);
         }
-      },
+    },
     setLoading({ commit }, payload) {
         commit("setLoading", payload);
-    }
+    },
+    // async getOPG({ commit }, payload) {
+    //     console.log(payload)
+    //     const data = await this.$axios.$get(`/api/records/${payload}`);
+    //     // const data = await this.$axios.$get(`/api/records/e2cb511d-5e05-4fa4-8bea-cc77b68c1c54`);
+    //     commit("setOGP", data);
+    // }
+    async getOPG({ commit }, payload) {
+        console.log(payload);
+        const data = await this.$axios.$get(`/api/records/${payload}`);
+        commit("setOGP", data);
+      }
 };
 
 export const getters = {
@@ -49,5 +60,8 @@ export const getters = {
     },
     loading(state) {
         return state.loading;
+    },
+    ogpData(state) {
+        return state.ogpData;
     }
 };
